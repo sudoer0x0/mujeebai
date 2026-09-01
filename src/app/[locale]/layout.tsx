b694c4ef49bd01edde6ee14d1751055608a620f4
mujeeb <yungswag111@gmail.com>
2026-09-01 23:56:13 +0100

    feat: use in-app brand logo for all favicons and icons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -50,6 +50,7 @@ export async function generateMetadata({
     icons: {
       icon: [
         { url: "/favicon.ico", sizes: "any" },
+        { url: "/favicon.svg", type: "image/svg+xml" },
         { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
         { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
         { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
